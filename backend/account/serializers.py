bb9d72a91d4852ab4a8e0974611e18817eb0b933
Bartek7655 <bartekk194@gmail.com>
2023-04-15 15:52:34 +0200

    create account

---
+++ backend/account/serializers.py
@@ -0,0 +1,34 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=UniqueValidator(queryset=get_user_model().objects.all())
+    )
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password],
+        trim_whitespace=False,
+        style={"input_type": "password"}
+    )
+    password2 = serializers.CharField(
+        write_only=True,
+        required=True,
+        trim_whitespace=False,
+        style={"input_type": "password"}
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'password', 'password2')
+
+    def validate(self, attrs):
+        if len(attrs.get("password")) < 5:
+            raise serializers.ValidationError(
+
+            )
--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from . import views
 
 urlpatterns = [
-
+    path('signin/', TokenObtainPairView.as_view(), name='signin_with_token'),
+    path('signup/', views.RegisterView.as_view(), name='register'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
